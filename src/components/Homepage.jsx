62e991e6e6f4a842a66da99d0946146fa5af4f3c
augusto <asbaez@outlook.com>
2023-11-03 17:32:00 -0300

    add testimonials

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -93,6 +93,60 @@ const Homepage = () => {
                 </div>
             </div>
 
+            <div className='testimonials'>
+                {/*                 <img src="images/bg-quotes.png" alt="quotes" /> */}
+                <div className='testimonialBlock'>
+                    <p>
+                        Fylo has improved our team productivity by an order of magnitude. Since making the switch our team has
+                        become a well-oiled collaboration machine.
+                    </p>
+                    <div className='profile'>
+                        <img src="images/profile-1.jpg" alt="profile pic" height="45px" />
+                        <div>
+                            <p>
+                                <b>
+                                    Satish Patel
+                                </b>
+                            </p>
+                            <p>Founder & CEO, Huddle</p>
+                        </div>
+                    </div>
+                </div>
+                <div className='testimonialBlock'>
+                    <p>
+                        Fylo has improved our team productivity by an order of magnitude. Since making the switch our team has
+                        become a well-oiled collaboration machine.
+                    </p>
+                    <div className='profile'>
+                        <img src="images/profile-2.jpg" alt="profile pic" height="45px" />
+                        <div>
+                            <p>
+                                <b>
+                                    Bruce McKenzie
+                                </b>
+                            </p>
+                            <p>Founder & CEO, Huddle</p>
+                        </div>
+                    </div>
+                </div>
+                <div className='testimonialBlock'>
+                    <p>
+                        Fylo has improved our team productivity by an order of magnitude. Since making the switch our team has
+                        become a well-oiled collaboration machine.
+                    </p>
+                    <div className='profile'>
+                        <img src="images/profile-3.jpg" alt="profile pic" height="45px" />
+                        <div>
+                            <p>
+                                <b>
+                                    Iva Boyd
+                                </b>
+                            </p>
+                            <p>Founder & CEO, Huddle</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
